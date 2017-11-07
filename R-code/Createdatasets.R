ab4d2535c7b391ccdfa21d5b721e4e744290bec6
marcosamaris <marcos.amaris@gmail.com>
2017-11-07 00:15:55 -0800

    Uploaded some images of the experiments with the features

---
+++ R-code/Createdatasets.R
@@ -0,0 +1,318 @@
+library(Hmisc)
+library(zoo)
+library(plyr)
+library(mlbench)
+library(e1071)
+library(randomForest)
+
+dirpath <- "~/rodiniaSched/"
+setwd(paste(dirpath, sep=""))
+
+source("./R-code/include/common.R")
+source("./R-code/include/sharedFunctions.R")
+
+set.seed(5)
+
+# tempTime <- matrix()
+# for(i in 1:2){
+#   temp <- read.csv(paste("./data/backprop-GPU-", i, ".csv", sep = ""), header = FALSE)
+#   tempTime <- cbind(temp[grep("adjust", temp$V2),]["V4"], tempTime)
+# }
+# 
+# tempTime$tempTime <- NULL
+# duration <- rowMeans(tempTime)
+
+kernel <- names(kernelsDict[3])
+gpu <- 2
+
+for(kernel in 3:8){
+  for(gpu in 2){#NoGPU){  
+    temp <- data.frame(read.csv(paste("./datasets/", names(kernelsDict[kernel]), "-", gpus[gpu,"gpu_name"], ".csv", sep = "")))
+    tempNvprof <- temp[, which(names(temp) %in% c("duration", "input.size.1", "input.size.2"))]
+    
+    temp <- read.csv(paste("./data/", kernelsDict[kernel], "-GPU.csv", sep = ""), header = FALSE,stringsAsFactors = FALSE)
+    temp <- temp[grep(names(kernelsDict[kernel]), temp$V2),]
+      
+    # data <- data.frame(tempNvprof, temp[,3:(dim(temp)[2]-2)])
+    
+    if (kernelsDict[kernel] == "backprop"){
+      
+      # names(data) <- c("P1") 
+      trainingData <- data[data$input.size.1 <= 27648, -c(1,2)][-1,]
+      testingData <- data[data$input.size.1 >= 27648,-c(1,2)][-1,]
+    }
+    
+    if (kernelsDict[kernel] == "gaussian"){
+      # temp$V3 <- as.numeric(temp$V3)
+      temp <- temp[temp$V3 <= 4096,]
+      # data <- data.frame(tempNvprof, temp[,3:(dim(temp)[2]-2)])
+      data <- data.frame(tempNvprof, sapply(temp[,3:(dim(temp)[2]-2)], normalizeMax))
+      trainingData <- data[data$input.size.1 <= 2048, -c(1,2)]
+      testingData <- data[data$input.size.1 == 4096,-c(1,2)]
+    }
+    
+    if (kernelsDict[kernel] == "kernel"){
+      # temp$V3 <- as.numeric(temp$V3)
+      # data <- data.frame(tempNvprof, temp[,3:(dim(temp)[2]-2)])
+      trainingData <- data[data$input.size.1 == 25, -c(1,2)][-1,]
+      testingData <- data[data$input.size.1 == 100,-c(1,2)][-1,]
+    }
+    
+    if (kernelsDict[kernel] == "calculate_temp"){
+      # data <- data.frame(tempNvprof, temp[,3:(dim(temp)[2]-2)])
+      trainingData <- data[data$input.size.1 <= 256 & data$input.size.2 == 256, -c(1,2)][-1,]
+      testingData <-  data[data$input.size.1 >= 256 & data$input.size.2 != 256, -c(1,2)][-1,]
+    }
+    if (kernelsDict[kernel] == "hotspotOpt1"){
+      # temp$V3 <- as.numeric(temp$V3)
+      # data <- data.frame(tempNvprof, sapply(temp[,3:(dim(temp)[2]-2)], normalizeMax))
+      trainingData <- data[data$input.size.1 <= 4 & data$input.size.2 <= 100, -c(1,2)][-1,]
+      testingData <- data[data$input.size.1 > 4 & data$input.size.2 > 100,-c(1,2)][-1,]
+    }
+    if (kernelsDict[kernel] == "needle_cuda_shared_1" | kernelsDict[kernel] == "needle_cuda_shared_2"){
+      # temp$V3 <- as.numeric(temp$V3)
+      # data <- data.frame(tempNvprof, temp[,3:(dim(temp)[2]-2)])
+      # data <- data.frame(tempNvprof, sapply(temp[,3:(dim(temp)[2]-2)], normalizeMax))
+      
+      trainingData <- data[data$input.size.1 <= 1024 & data$input.size.2 <= 4, -c(1,2)][-1,]
+      testingData <- data[data$input.size.1 == 4096 & data$input.size.2 > 4,-c(1,2)][-1,]
+    }
+    
+   predictKernelsLM(trainingData, testingData)
+      
+  }
+}
+
+
+createCounter <- function(value) { function(i) { value <<- value+i} }
+
+predictKernelsLM <- function(trainingData, testingData){
+  
+  counter <- createCounter(0)
+  rss <- matrix(nrow = 12, ncol = 3)
+  png(filename = paste("./images/fitModels/", names(kernelsDict[kernel]), ".png", sep=""), width = 1600, height = 800)
+  par(family = "Times", mfrow=c(3,3))
+  for(resultY in c("","log2")){
+    for(FeaturesX in c("", "log", "exp", "poly")){
+      count <- counter(1)
+      fit <- lm(as.formula(
+          paste(resultY, "(", names(trainFeatures[i]),  ifelse(resultY == "log2", "+ 0.0000000001", ""),") ~",
+                paste(FeaturesX, "(",ifelse(FeaturesX == "exp", "normalizeMax(",""),  
+                      colnames(trainFeatures[!names(trainFeatures) == i]), ifelse(FeaturesX == "poly", ",3,raw = TRUE)",
+                                                                                  ifelse(FeaturesX == "log2", "+ 0.0000000001)", ")")),ifelse(FeaturesX == "exp", ")", "") , 
+                      collapse = " + " ))), data =  trainFeatures)
+      
+      rss[count,1] <- summary(fit)$adj.r.squared
+      rss[count,2] <- resultY
+      rss[count,3] <- FeaturesX
+      
+      base <- residuals(fit)
+      qqnorm(base, ylab="Studentized Residual",
+             xlab="t Quantiles",
+             main=paste(" Y=", resultY, " X=", FeaturesX, sep=""),
+             cex.lab = 2, cex.main=2,cex=1.5,cex.axis=2)
+      qqline(base, col = 2,lwd=5)
+    }
+  }
+  dev.off()
+  rss
+ 
+  key <- which(rss[,1] == max(rss[,1]))[1]
+  
+  timeKernel <- predict(lm(as.formula(paste(rss[key, 2], "(", names(trainFeatures[i]),  ") ~",
+                                                  paste(rss[key,3], "(",ifelse(rss[key,3] == "exp", "normalizeMax(",""),  
+                                                        colnames(trainFeatures[!names(trainFeatures) == i]), ifelse(FeaturesX == "poly", ",3,raw = TRUE)",
+                                                                                                                    ifelse(rss[key,3] == "log", "+ 0.0000000001)", ")")),ifelse(rss[key,3] == "exp", ")", "") , 
+                                                        collapse = " + " ))
+  ), data =  trainFeatures), testFeatures)
+  
+  # timeKernel <- predict(randomForest(names(trainFeatures[i])~., data =  trainingData), testingData[,-1])
+  
+  if (rss[key, 2] == "log2")
+      timeKernel <- 2^(timeKernel) - 0.0000000001 
+  
+  
+  png(filename = paste("./images/fakeFeatures/", names(kernelsDict[kernel]), ".png", sep=""), width = 1600, height = 800)
+    par(mfrow=c(1,2))
+  plot(testingData[,1], ylim = c(0, max(timeKernel,testingData[,1])),  col="blue", ylab = "Time ms", cex=2,
+       main=paste(names(kernelsDict[kernel]), "|  ", rss[key,2], "(y) = ", rss[key,3], "(x)"))
+  legend("bottomleft", legend = c("Measured", "Predicted"), col = c("blue", "red"),lwd = 2, cex = 2) 
+    points(timeKernel, col="red")
+    boxplot(testingData[,1]/timeKernel, ylab="Accuracy. Measured/Predicted", cex=2)
+    # title(paste(kernel, "|  ", rss[key,2], "(Y) = ", rss[key,3], "(X)"), outer=TRUE) 
+  dev.off()
+}
+
+
+
+# predictParametersTraining <- function(trainingData, testingData){
+#   fakeFeature <- data.frame(matrix(ncol=0,nrow=dim(testingData)[1]),stringsAsFactors=FALSE, check.names = FALSE)
+#   for (feature in 3:7){  
+#     counter <- createCounter(0)
+#     
+#     rss <- matrix(nrow = 9, ncol = 3)
+#     png(filename = paste("./images/fitModels/", colnames(trainingData[feature]), ".png", sep=""), width = 1600, height = 800)
+#     par(family = "Times", mfrow=c(3,3))
+#     for(resultY in c("", "exp", "log")){
+#         for(FeaturesX in c("", "log", "exp", "poly")){
+#           count <- counter(1)
+#             try(fit <-lm(as.formula(paste(resultY, "(",colnames(trainingData[feature]) , ") ~", 
+#                     paste(FeaturesX, "(",colnames(trainingData[,1:2]),")",collapse = " + ")
+#               )), data =  trainingData), TRUE)
+#             rss[count,1] <- mean(fit$residuals)
+#             rss[count,2] <- resultY
+#             rss[count,3] <- FeaturesX
+#             
+#             base <- residuals(fit)
+#             qqnorm(base, ylab="Studentized Residual",
+#                    xlab="t Quantiles",
+#                    main=paste(colnames(trainingData[feature]), " Y=", resultY, " X=", FeaturesX, sep=""),
+#                    cex.lab = 2, cex.main=2,cex=1.5,cex.axis=2)
+#             qqline(base, col = 2,lwd=5)
+# 
+#         }
+#     }
+#     dev.off()
+#     key <- which(rss[,1] == max(rss[,1]))[1]
+#     
+#     tempFeature <- predict(lm(as.formula(paste(rss[key,2], "(",colnames(trainingData[feature]) , ") ~", 
+#                               paste(rss[key,3], "(",colnames(trainingData[,1:2]),")",collapse = " + ")
+#     )), data =  trainingData), testingData[,1:2])[1:27]
+#   
+#     if(rss[key,2] == "exp")
+#       tempFeature <- log(tempFeature)
+#     
+#     if(rss[key,2] == "log")
+#       tempFeature <- exp(tempFeature)
+#     
+#     if(rss[key,2] == "")
+#       tempFeature <- tempFeature
+#     
+#     fakeFeature <- cbind(fakeFeature, tempFeature)
+#   }
+#   names(fakeFeature) <- names(testingData[3:7])  
+#   
+#   png(filename = paste("./images/fakeFeatures/", kernel, ".png", sep=""), width = 1600, height = 800)
+#   par(family = "Times", mfrow=c(3,2))
+#   for(i in 1:5){
+#     plot(testingData[,i+2], col="blue", ylab = names(testingData[i+2]))
+#     lines(fakeFeature[,i], col="red")
+#   }
+#   dev.off()
+#   return(fakeFeature)
+# }
+
+# 
+# 
+# # input<-seq(from=8192, by = 1024, to = 262144) 
+# testData <- data.frame(
+# elapsed_cycles_sm=approxExtrap(backprop$input.size.1[1:57],backprop$elapsed_cycles_sm[1:57],xout=input[58:249])$y,
+# device_memory_read_transactions=approxExtrap(backprop$input.size.1[1:57],backprop$device_memory_read_transactions[1:57],xout=input[58:249])$y,
+# global_store_transactions=approxExtrap(backprop$input.size.1[1:57],backprop$global_store_transactions[1:57],xout=input[58:249])$y,
+# issued_control.flow_instructions=approxExtrap(backprop$input.size.1[1:57],backprop$issued_control.flow_instructions[1:57],xout=input[58:249])$y)
+# 
+# MA <- trainingData$multiprocessor_activity
+# 
+# trainingData$multiprocessor_activity <- NULL 
+# 
+# # fit <- lda(trainingData$multiprocessor_activity ~., data =  trainingData)
+# # fit <- qda(trainingData$multiprocessor_activity ~., data =  trainingData)
+# 
+# fit <- randomForest(MA ~ ., data = trainingData, mtry=2, ntree=20)
+# createdMA <- predict(fit, testData)
+# plot(c(MA,createdMA))
+# 
+# 
+# 
+# 
+# 
+# createdMA <- predict(fit, testData)
+# createdMA <- log(createdMA)
+# createdMA[createdMA > 100] <- 100
+# testData$multiprocessor_activity <- createdMA
+# 
+# testFinalData <- rbind(trainingData, testData)
+# 
+#   fit <- lm(duration[1:124] ~ ., data = testFinalData[1:124,])
+#   predictions <- predict(fit, testFinalData[125:249,])
+# 
+#     boxplot(duration[125:249]/predictions, main=names(testFinalData))
+# 
+# 
+# mape <- mean(predictions - duration[58:249])/abs(duration[58:249])*100
+# mape
+# 
+# # [7]  multiprocessor_activity 
+# # [1]  elapsed_cycles_sm
+# # [10] device_memory_read_transactions 
+# # [9]  global_store_transactions      
+# # [13] issued_control.flow_instructions
+# 
+# # [19] load.store_instructions
+# # [16] executed_load.store_instructions
+# # [6]  active_cycles 
+# # [20] misc_instructions 
+# # [18] control.flow_instructions 
+# 
+# temp <- read.csv(paste("./data/gaussian-GPU.csv", sep = ""), header = FALSE,stringsAsFactors = FALSE)
+# tempTime <- temp[grep("Fan1", temp$V2),]
+# tempTime$V3 <- as.numeric(tempTime$V3)
+# 
+# # plot(tempTime$V5[tempTime$V3 == 2048]/1000000)
+# 
+# gauss <- data.frame(read.csv("./datasets/Fan1-Tesla-K40.csv"))
+# features <- gauss[, which(names(gauss) %in% c("duration","input.size.1", "multiprocessor_activity",  
+#                                                         "elapsed_cycles_sm",  
+#                                                         "device_memory_read_transactions",
+#                                                         "global_store_transactions",
+#                                                         "issued_control.flow_instructions"))]
+# iterNumber <- count(features$input.size.1)$x
+# 
+# for(sizeMatrix in c(256, 512, 1024, 2048)){
+#   # print(i)
+#   trainingData <- features[features$input.size.1 == sizeMatrix,]
+#   trainingData$input.size.1 <- NULL
+#   trainingData$elapsed_cycles_sm <- NULL
+#   trainingData$multiprocessor_activity <- NULL
+#   trainingData$device_memory_read_transactions <-NULL 
+#   testData <- data.frame(
+#             # device_memory_read_transactions=approxExtrap(tempTime$V4[tempTime$V3 == sizeMatrix],gauss$device_memory_read_transactions[gauss$input.size.1==sizeMatrix],xout=c(tempTime$V4[tempTime$V3 == sizeMatrix+256], 1))$y
+#             global_store_transactions=approxExtrap(tempTime$V4[tempTime$V3 == sizeMatrix],gauss$global_store_transactions[gauss$input.size.1==sizeMatrix],xout=c(tempTime$V4[tempTime$V3 == sizeMatrix*256], 1))$y,
+#             issued_control.flow_instructions=approxExtrap(tempTime$V4[tempTime$V3 == sizeMatrix],gauss$issued_control.flow_instructions[gauss$input.size.1==sizeMatrix],xout=c(tempTime$V4[tempTime$V3 == sizeMatrix*256], 1))$y
+#   )
+#   
+#    # elapsed_cycles_sm=approxExtrap(tempTime$V4[tempTime$V3 == sizeMatrix],gauss$elapsed_cycles_sm[gauss$input.size.1==sizeMatrix],xout=tempTime$V4[tempTime$V3 == sizeMatrix+256])$y
+#   fit <- lm(trainingData$duration ~ ., data = trainingData)
+#   predictions <- predict(fit, testData) 
+#   measured <- features$duration[features$input.size.1 == sizeMatrix*2]
+#   length(predictions)
+#   boxplot(predictions/measured, main=paste("Training=",sizeMatrix, "  Testing=",sizeMatrix*2, sep=""))
+# }
+# gauss <- data.frame(read.csv("./datasets/Fan1-Tesla-K40.csv"))
+# trainingData <- gauss[, which(names(gauss) %in% c("duration","input.size.1",  
+#                                                   "elapsed_cycles_sm",  "device_memory_read_transactions", "global_store_transactions",
+#                                                   "issued_control.flow_instructions"))]
+# 
+# trainingData <- trainingData[trainingData$input.size.1 == 4096,]
+# trainingData$input.size.1 <- NULL
+# 
+# trainingData$duration <- NULL
+# 
+# 
+# predictions <- predict(fit, trainingData)
+# length(predictions)
+# 
+# duration <- tempTime$V5[tempTime$V3 == 2048]
+# length(duration)
+# boxplot(predictions/tempTime$V5[tempTime$V3 == 4096])
+# 
+# mape <- mean(predictions - tempTime$V5[tempTime$V3 == 6144])/abs(tempTime$V5[tempTime$V3 == 6144])*100
+# 
+# length(tempTime$V5[tempTime$V3 == 2048])
+# dim(trainingData)
+# 
+# 
+# plot(trainingData$duration[trainingData$input.size.1 == 2048])
+# 
+# 
+# View(tempTime)
---
+++ R-code/include/common.R
@@ -0,0 +1,48 @@
+
+gpus <- read.table("./data/deviceInfo.csv", sep=",", header=T)
+
+cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+apps <- c("backprop", "gaussian", "heartwall",  "hotspot", "hotspot3D", "lavaMD", "lud", 
+          "needle","matMul_gpu_uncoalesced","matMul_gpu", "matMul_gpu_sharedmem_uncoalesced", "matMul_gpu_sharedmem",
+          "matrix_sum_normal", "matrix_sum_coalesced", 
+          "dotProd", "vectorAdd",  "subSeqMax") 
+
+kernelsDict <- vector(mode="list", length=22)
+  names(kernelsDict) <- c("bpnn_layerforward_CUDA",
+                        "bpnn_adjust_weights_cuda",
+                        "Fan1",
+                        "Fan2",
+                        "kernel",
+                        "calculate_temp",
+                        "hotspotOpt1",
+                        "kernel_gpu_cuda",
+                        "lud_diagonal",
+                        "lud_perimeter",
+                        "lud_internal",
+                        "needle_cuda_shared_1",
+                        "needle_cuda_shared_2",
+                        "matMul_gpu_uncoalesced","matMul_gpu", "matMul_gpu_sharedmem_uncoalesced", "matMul_gpu_sharedmem",
+                          "matrix_sum_normal", "matrix_sum_coalesced", 
+                          "dotProd", "vectorAdd",  "subSeqMax")
+
+kernelsDict[[1]] <- apps[1]
+kernelsDict[[2]] <- apps[1]
+kernelsDict[[3]] <- apps[2]
+kernelsDict[[4]] <- apps[2]
+kernelsDict[[5]] <- apps[3]
+kernelsDict[[6]] <- apps[4]
+kernelsDict[[7]] <- apps[5]
+kernelsDict[[8]] <- apps[6]
+kernelsDict[[9]] <- apps[7]
+kernelsDict[[10]] <- apps[7]
+kernelsDict[[11]] <- apps[7]
+kernelsDict[[12]] <- apps[8]
+kernelsDict[[13]] <- apps[8]
+
+for(i in 14:22)
+  kernelsDict[[i]] <- apps[i-5]
+
+
+kernel_1_parameter <- c(1,2,3,4,5,8,9,10,11)
+kernel_2_parameter <- c(6,7,12,13)
